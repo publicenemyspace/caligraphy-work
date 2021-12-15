0e54a95cd9a83b54ccc05cc1d4c0fc069662ccc0
publicenemyspace <cupidspaces@gmail.com>
2021-12-15 12:16:21 +0800

    caligraphy work

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+      function initialize() {
+
+            var num = new google.maps.LatLng(9.0406526, 7.503654699999999);
+
+            var myOptions = {
+                zoom: 16,
+                center: num,
+                mapTypeId: google.maps.MapTypeId.HYBRID
+            };
+
+            var mymap = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            var marker = new google.maps.Marker({
+                position: num,
+                map: mymap,
+                title: "Aptech Garki ABuja!"
+            });
+        } 
+ var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("demo");
+      var captionText = document.getElementById("caption");
+      if (n > slides.length) { slideIndex = 1 }
+      if (n < 1) { slideIndex = slides.length }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+      captionText.innerHTML = dots[slideIndex - 1].alt;
+    }
